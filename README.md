fbbdf137d0931eb59968e021bf4c94b73050e17e
Patty Neris <pneris13@gmail.com>
2021-07-21 21:35:29 -0300

    create e2e seat selection

---
+++ README.md
@@ -0,0 +1,28 @@
+Challenge: Write a selenium scraper that takes two seat numbers as input,
+selects the seats from this page (https://static.gordiansoftware.com/)
+and then takes a screenshot of the "Seat Selection" element.
+The output should look something like the attached image.  
+It should work if the selected seat is a regular seat, and also if it's on an exit row."
+
+
+
+Setup of evironment:
+1. Install Chocolatey (package manager)
+2. Install Python
+3. Install Selenium and Selenium drivers all
+4. Install Chromedriver
+
+How run project:
+
+# Windows:
+Step 1: clone the project to pc
+Step 2: In PowerShell or CMD, access path until "/gordian"
+Step 3: Run: $ .\virtual\Scripts\activate.ps1
+Step 4: Run: $ python test.py
+
+# Linux/Unix:
+Step 1: clone the project to pc
+Step 2: In CMD, access path until "/gordian"
+Step 3: Run: $ .\virtual\Scripts\activate
+Step 4: Run: $ python test.py
+
---
+++ test.py
@@ -0,0 +1,49 @@
+from selenium.webdriver import Chrome
+from time import sleep
+from PIL import Image
+
+
+url = 'https://static.gordiansoftware.com/'
+width = 1291
+height = 144
+x = 23
+y = 1126
+
+driver = Chrome()
+
+driver.set_window_size(1366, 768)
+
+driver.get(url)
+
+sleep(2)
+
+driver.find_element_by_xpath('//*[@id="gordian-compartments"]/div[7]/div[1]/button[2]').click()
+
+driver.find_element_by_xpath('//*[@id="select-seat"]').click()
+
+driver.find_element_by_xpath('//*[@id="next-button"]').click()
+
+sleep(2)
+
+driver.find_element_by_xpath('//*[@id="gordian-compartments"]/div[7]/div[1]/button[3]').click()
+
+driver.find_element_by_xpath('//*[@id="select-seat"]').click()
+
+driver.find_element_by_xpath('//*[@id="next-button"]').click()
+
+sleep(7)
+
+driver.execute_script("window.scrollBy(0,768)", "")
+
+sleep(7)
+
+driver.save_screenshot('Finish Selection.png')
+
+# x = location['x'];
+# y = location['y'];
+# width = location['x']+size['width'];
+# height = location['y']+size['height'];
+im = Image.open('Finish Selection.png')
+im = im.crop((int(x), int(y), int(width), int(height)))
+im.save('Seat Selection.png')
+# browser.quit()
