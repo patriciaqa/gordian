aa1066753c02a35bdd148e4f24b879f3c1762c4b
Patty Neris <pneris13@gmail.com>
2021-07-22 15:00:31 -0300

    clear code

--- test.py
+++ test.py
@@ -4,66 +4,40 @@ from PIL import Image
 import cv2
 import numpy as np
 
+#setting browser
 url = 'https://static.gordiansoftware.com/'
 
 driver = Chrome()
 
-# driver.maximize_window()
 driver.set_window_size(1366, 768)
-
 
 driver.get(url)
 
 sleep(2)
 
+# Seat selection
 driver.find_element_by_xpath('//*[@id="gordian-compartments"]/div[7]/div[1]/button[2]').click()
-
 driver.find_element_by_xpath('//*[@id="select-seat"]').click()
-
 driver.find_element_by_xpath('//*[@id="next-button"]').click()
 
 sleep(2)
 
 driver.find_element_by_xpath('//*[@id="gordian-compartments"]/div[7]/div[1]/button[3]').click()
-
 driver.find_element_by_xpath('//*[@id="select-seat"]').click()
-
 driver.find_element_by_xpath('//*[@id="next-button"]').click()
 
+# Screenshot
 sleep(3)
 driver.execute_script("window.scrollBy(0,768)", "")
-
 sleep(3)
 driver.save_screenshot('Seat Selection.png')
 
-
-# element =  driver.find_element_by_id('trigger')
-# location = element.location
-# size = element.size
-
 sleep(2)
-# #to get the x axis
-# x = location['x']
-# print(x)
-# #to get the y axis
-# y = location['y']
-# print(y)
-# # to get the length the element
-# height = location['y']+size['height']
-# print(height)
-# # to get the width the element
-# width = location['x']+size['width']
-# print(width)
 
 img = cv2.imread('Seat Selection.png')
-print(img.shape) # Print image shape
-# cv2.imshow("original", img)
 
 # Cropping an image
 cropped_image = img[420:620, 3:1325]
-
-# Display cropped image
-# cv2.imshow("cropped", cropped_image)
 
 # Save the cropped image
 cv2.imwrite("Seat Selection.png", cropped_image)
